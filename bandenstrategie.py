a20dd76719a1dace0c46f0c05f42e1742d7053d2
Karam B <113892735+Karambm@users.noreply.github.com>
2023-11-13 13:40:24 +0100

    drie

--- bandenstrategie.py
+++ bandenstrategie.py
@@ -6,34 +6,20 @@ session = fastf1.get_session(2023, "Bahrain", 'R')
 session.load()
 laps = session.laps
 
-###############################################################################
-# Get the list of driver numbers
 drivers = session.drivers
 print(drivers)
 
-###############################################################################
-# Convert the driver numbers to three letter abbreviations
 drivers = [session.get_driver(driver)["Abbreviation"] for driver in drivers]
 print(drivers)
 
-###############################################################################
-# We need to find the stint length and compound used
-# for every stint by every driver.
-# We do this by first grouping the laps by the driver,
-# the stint number, and the compound.
-# And then counting the number of laps in each group.
 stints = laps[["Driver", "Stint", "Compound", "LapNumber"]]
 stints = stints.groupby(["Driver", "Stint", "Compound"])
 stints = stints.count().reset_index()
 
-###############################################################################
-# The number in the LapNumber column now stands for the number of observations
-# in that group aka the stint length.
+
 stints = stints.rename(columns={"LapNumber": "StintLength"})
 print(stints)
 
-###############################################################################
-# Now we can plot the strategies for each driver
 fig, ax = plt.subplots(figsize=(5, 10))
 
 for driver in drivers:
@@ -41,8 +27,6 @@ for driver in drivers:
 
     previous_stint_end = 0
     for idx, row in driver_stints.iterrows():
-        # each row contains the compound name and stint length
-        # we can use these information to draw horizontal bars
         plt.barh(
             y=driver,
             width=row["StintLength"],
@@ -54,20 +38,12 @@ for driver in drivers:
 
         previous_stint_end += row["StintLength"]
 
-# sphinx_gallery_defer_figures
 
-###############################################################################
-# Make the plot more readable and intuitive
 plt.title("2022 Hungarian Grand Prix Strategies")
 plt.xlabel("Lap Number")
 plt.grid(False)
-# invert the y-axis so drivers that finish higher are closer to the top
 ax.invert_yaxis()
 
-# sphinx_gallery_defer_figures
-
-###############################################################################
-# Plot aesthetics
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.spines['left'].set_visible(False)
--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-import pandas as pd
 import filewriters as fw
-import loaders as l
-import macrofunctions as mf
-from time import perf_counter
-import os
+import bandenstrategie as l
+import pandas as pd
 
 l.fastf1.plotting.setup_mpl()
+
+
+def allcardata(session, combinedcardata=[]):
+    """
+    # TODO: Beschrijving
+    """
+    for driver in session.drivers:
+        lap = 1
+        while lap <= session.total_laps:
+            try:
+                lstdriver, x = [], 1
+                df = session.laps.pick_driver(
+                    driver).pick_lap(int(lap)).get_car_data()
+                while x <= df.Brake.size:
+                    lstdriver.append(driver)
+                    x += 1
+                df["driverID"] = lstdriver
+                combinedcardata.append(df)
+                # print(session.laps.pick_driver(str(driver)).pick_lap(int(lap)).get_car_data())
+            except KeyError as ke:
+                print(f"\x1b[31mKeyError: {ke}\x1b[0m")
+            except ValueError as ve:
+                print(f"\x1b[31mValueError: {ve}\x1b[0m")
+            lap += 1
+    return pd.concat(combinedcardata)
+
+
+def alllapdata(session, lap=1, combinedlapdata=[]):
+    """
+    # TODO: Beschrijving
+    """
+    while lap <= session.total_laps:
+        lapinfo = l.loadlap(session, lap)
+        combinedlapdata.append(lapinfo)
+        lap += 1
+    return pd.concat(combinedlapdata)
 
 
 def circuit_info(session):
@@ -18,79 +51,33 @@ def circuit_info(session):
         print(f'\x1b[31mTypeError: {te}\x1b[0m')
 
 
-def writeallsingle(session, year):
-    """
-    Schrijft alles gedefinieerd naar csv.
-
-    Result
-    - CSV-bestanden van alles gedefinieerd, waaronder telemetries; etc.
-    """
-    fw.writecsv(f'schedule_{year}', l.loadschedule(year)) # Writes the schedule of the given year
-    circuit_info(session) # Writes available circuit information containing cornering; marshals.
-    fw.writecsv("lapdata", mf.alllapdata(session)) # Writes all available lap data of the requested session
-    fw.writecsv("cardata", mf.allcardata(session)) # Writes all available car data of the requested session
-    fw.writecsv('remaining_sessions', l.loadremaining()) # Writes the remaining sessions of the season
-    fw.writecsv('weather_data', session.weather_data) # Writes weather data
-    fw.writecsv('track_status', session.track_status) # Writes track status which contains green; yellow flags; safety cars
-
+def main(session):
     # SESSION RESULTS
     fw.writecsv('session_results', session.results)
-
-
-def writeallseason(year):
-    """
-    - Laps
-    - Cars
-    - Weather
-    """
-    # CARDATA
-    try:
-        fw.writecsv(f'cardata_{year}', mf.seasoncardata(year, sprinttype))
-    except UnboundLocalError as ule:
-        print(f"\x1b[31m{ule}\x1b[0m")
     pass
 
-    # LAPDATA
-    try:
-        fw.writecsv(f'lapdata_{year}', mf.seasonlapdata(year, sprinttype))
-    except UnboundLocalError as ule:
-        print(f"\x1b[31m{ule}\x1b[0m")
-    pass
-
-
-"""
-TODO's:
-- DONE: Load a session with given parameters, which can call other actions.
-- DONE: Race Schedule for a given season.
-- DONE: Remaining Races given the latest season.
-- DONE: Aquire lap data to which information as position and laptimes are given.
-- DONE: An aggregated list of car data per lap per driver.
-- DONE: Acquire weather data of the given session.
-- TODO: Write all available data iteratively to csv.
-- TODO: Match all columns with obtained datasets via ergast.
-"""
 
 if __name__ == "__main__":
-    year, location, sprinttype = 2023, 'monza', 'R'
+    year, location, sprinttype = 2023, 'Bahrain', 'R'
 
-    # THE WHOLE SEASON
-    gocache = 'n'
-    if gocache == 'j':
-        try:
-            starttijd = perf_counter()
-            mf.cacheall(year)
-            print(f" Tijd: \x1b[31m{(perf_counter() - starttijd) * 1000:.0f}ms\x1b[32m)")
-        except:
-            pass
-    writeallseason(year)
-    
-    # ONE SESSION
     print("\x1b[32m")
     session = l.loadsession(year, location, sprinttype)  # Requests the session
     print("\x1b[0m")
 
-    # WRITE ONE SESSION
-    writeallsingle(session, year)
+    # Writes available circuit information containing cornering; marshals.
+    circuit_info(session)
+    # Writes all available lap data of the requested session
+    fw.writecsv("lapdata", alllapdata(session))
+    # Writes all available car data of the requested session
+    fw.writecsv("cardata", allcardata(session))
+    # Writes the schedule of the given year
+    fw.writecsv(f'schedule_{year}', l.loadschedule(year))
+    # Writes the remaining sessions of the season
+    fw.writecsv('remaining_sessions', l.loadremaining())
+    fw.writecsv('weather_data', session.weather_data)  # Writes weather data
+    # Writes track status which contains green; yellow flags; safety cars
+    fw.writecsv('track_status', session.track_status)
 
+    main(session)
 
 # l.fastf1.Cache.clear_cache()
