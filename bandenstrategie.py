b0e08683eea8d2d91210c6212c40997e8a4f376c
Karam B <113892735+Karambm@users.noreply.github.com>
2023-11-13 13:06:47 +0100

    Update bandenstrategie.py

--- bandenstrategie.py
+++ bandenstrategie.py
@@ -1,58 +1,77 @@
 import fastf1
 import fastf1.plotting
+from matplotlib import pyplot as plt
 
+session = fastf1.get_session(2022, "Hungary", 'R')
+session.load()
+laps = session.laps
 
-def loadcardata(session, driver, lap):
-    """
-    #TODO BESCHRIJVING
+###############################################################################
+# Get the list of driver numbers
+drivers = session.drivers
+print(drivers)
 
-    Results:
-    - Telemetry (DataFrame): information regarding the car within a given lap.
-    """
-    return session.laps.pick_driver(str(driver)).pick_lap(int(lap)).get_car_data()
+###############################################################################
+# Convert the driver numbers to three letter abbreviations
+drivers = [session.get_driver(driver)["Abbreviation"] for driver in drivers]
+print(drivers)
 
+###############################################################################
+# We need to find the stint length and compound used
+# for every stint by every driver.
+# We do this by first grouping the laps by the driver,
+# the stint number, and the compound.
+# And then counting the number of laps in each group.
+stints = laps[["Driver", "Stint", "Compound", "LapNumber"]]
+stints = stints.groupby(["Driver", "Stint", "Compound"])
+stints = stints.count().reset_index()
 
-def loadlap(session, lap):
-    """
-    #TODO BESCHRIJVING
+###############################################################################
+# The number in the LapNumber column now stands for the number of observations
+# in that group aka the stint length.
+stints = stints.rename(columns={"LapNumber": "StintLength"})
+print(stints)
 
-    Results:
-    - Lapinformation (DataFrame): 
-    """
-    return session.laps.pick_lap(lap)
+###############################################################################
+# Now we can plot the strategies for each driver
+fig, ax = plt.subplots(figsize=(5, 10))
 
+for driver in drivers:
+    driver_stints = stints.loc[stints["Driver"] == driver]
 
-def loadremaining():
-    """
-    Loads the remaining sessions given the latest year that's available.
+    previous_stint_end = 0
+    for idx, row in driver_stints.iterrows():
+        # each row contains the compound name and stint length
+        # we can use these information to draw horizontal bars
+        plt.barh(
+            y=driver,
+            width=row["StintLength"],
+            left=previous_stint_end,
+            color=fastf1.plotting.COMPOUND_COLORS[row["Compound"]],
+            edgecolor="black",
+            fill=True
+        )
 
-    Results:
-    - remaining (DataFrame): which contains the remaining sessions within the latest season.
-    """
-    return fastf1.get_events_remaining()
+        previous_stint_end += row["StintLength"]
 
+# sphinx_gallery_defer_figures
 
-def loadschedule(year):
-    """
-    Loads the schedule for a given year.
-    - year (int): the year of which the schedule is loaded for.
+###############################################################################
+# Make the plot more readable and intuitive
+plt.title("2022 Hungarian Grand Prix Strategies")
+plt.xlabel("Lap Number")
+plt.grid(False)
+# invert the y-axis so drivers that finish higher are closer to the top
+ax.invert_yaxis()
 
-    Results:
-    - schedule (DataFrame): the schedule of the given year.
-    """
-    return fastf1.get_event_schedule(int(year))
+# sphinx_gallery_defer_figures
 
+###############################################################################
+# Plot aesthetics
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_visible(False)
 
-def loadsession(year, circuit, sessiontype):
-    """
-    Loads a given session defined by:
-    - year (int): the year the race was held.
-    - circuit (name: str | id: int): the circuit which was raced on in the given year.
-    - sessiontype (str): which type of session it is (Qualifying, Race, etc.).
+plt.tight_layout()
+plt.show()
 
-    Results:
-    - session (DataFrame): which contains a variety of information regarding the requested session.
-    """
-    session = fastf1.get_session(year, circuit, sessiontype)
-    session.load(laps=True, weather=True, telemetry=True, messages=True)
-    return session
